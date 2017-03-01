cf4dbb42709873cbe1c44dc80d08fdcaaad537a8
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-02 01:42:00 +0700

    Load Food List in CalCal

--- src/js/components/calcal/CalCal.js
+++ src/js/components/calcal/CalCal.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import service from '../../firebase'
 import {Link} from 'react-router'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount(){
+    service.getFood((data)=>{
+      this.setState({
+        menu : data
+      })
+    })
+  }
+
   render(){
     return (
-      <Link  className="btn btn-info"  to={{ pathname:'/admin' }}>Admin</Link>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>CalCal Calories Calculator</h1>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            
+          </div>
+          <div className="col">
+
+          </div>
+        </div>
+        {/*<Link  className="btn btn-info"  to={{ pathname:'/admin' }}>Admin</Link>*/}
+      </div>
     )
   }
 }
