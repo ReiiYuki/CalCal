a786e5b11fdeaa63030298242ccae1dd4fb41ac3
Voraton Lertrattanapaisal <iguru.shou.5410070@gmail.com>
2017-03-02 00:48:25 +0700

    Add Food

--- src/js/components/admin/Admin.js
+++ src/js/components/admin/Admin.js
@@ -19,6 +19,15 @@ export default class Admin extends Component {
     })
   }
 
+  updateFood(e){
+    e.preventDefault()
+    const name = this.refs.form.name.value
+    const cal = this.refs.form.cal.value
+    service.updateFood(name,cal,this.state.name,()=>{
+      console.log("success");
+    })
+  }
+
   render(){
     return (
       <div>
@@ -34,13 +43,49 @@ export default class Admin extends Component {
             {!this.state.token?
               <div className="row">
                 <div className="col-12 text-center">
-                  <button type="button" className="btn btn-primary text-center" onClick={(e)=>this.authentication(e)}>Login With Facebook</button>
+                  <button type="button" className="btn btn-primary" onClick={(e)=>this.authentication(e)}>Login With Facebook</button>
                 </div>
               </div>:
-              <div className="row">
-                <div className="col-12 text-center">
-                  <h2 className="display4">Welcome, {this.state.name}</h2>
+              <div>
+                <div className="row">
+                  <div className="col text-center">
+                    <h3>Welcome, {this.state.name}</h3>
+                  </div>
                 </div>
+                <div className="row">
+                  <div className="col">
+                    <h4>Insert New Food</h4>
+                  </div>
+                </div>
+                <form className="row" ref="form">
+                    <div className="col-md-5">
+                      <div className="row">
+                        <div className="col">
+                          <label for="food-name">Food Name</label>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <input type="text" className="form-control" id="food-name" placeholder="Enter Food Name" name="name"/>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-5">
+                      <div className="row">
+                        <div className="col">
+                          <label for="food-cal">Food Calories</label>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <input type="number" className="form-control" id="food-cal" placeholder="Enter Food Calories" name="cal"/>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-2">
+                      <button type="button" className="btn btn-success" onClick={(e)=>this.updateFood(e)}>Add</button>
+                    </div>
+                </form>
               </div>
             }
           </div>
--- src/js/firebase.js
+++ src/js/firebase.js
@@ -5,6 +5,8 @@ firebase.initializeApp(config)
 
 const provider = new firebase.auth.FacebookAuthProvider()
 
+const db = firebase.database()
+
 const auth = (cb)=>{
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
@@ -15,16 +17,25 @@ const auth = (cb)=>{
     // ...
   }).catch(function(error) {
     // Handle Errors here.
-    let errorCode = error.code;
-    let errorMessage = error.message;
+    let errorCode = error.code
+    let errorMessage = error.message
     // The email of the user's account used.
-    let email = error.email;
+    let email = error.email
     // The firebase.auth.AuthCredential type that was used.
-    let credential = error.credential;
+    let credential = error.credential
     // ...
-  });
+  })
+}
+
+const updateFood = (name,cal,writer,cb)=>{
+  db.ref('food/'+name).set({
+    name : name,
+    cal : cal,
+    writer : writer
+  })
 }
 
 export default {
-  auth : auth
+  auth : auth,
+  updateFood : updateFood
 }
